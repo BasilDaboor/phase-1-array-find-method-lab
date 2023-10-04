53926e0d39c2fb275e28e1416dba19e21b5f503a
basil daboor <basildaboor12@gmail.com>
2023-10-04 08:47:18 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(element => element.result === "W");
+  
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
